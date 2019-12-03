544b579e19c7832542f48fe9acc4c14475bfbcd4
estefaneDuart7 <52675052+estefaneDuart7@users.noreply.github.com>
2019-12-02 22:45:45 -0300

    Add files via upload

---
+++ pweb2-desafio01/src/main/java/br/com/democracia/democraciaocinema/Controlador.java
@@ -0,0 +1,31 @@
+package br.com.democracia.democraciaocinema;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class Controlador{
+
+    @RequestMapping("/formulario")
+    public ModelAndView formulario() {
+        return new ModelAndView("formulario.html");
+    }
+    
+    @RequestMapping("/resultado")
+    public ModelAndView resultado(String nome, String senha){
+        ModelAndView mv = new ModelAndView("resultado.html");
+        String novaSenha = senha.replaceAll(" ", "");
+        String novoNome = nome.replaceAll(" ", "");
+        mv.addObject("nome", "Seu nome tem " + novoNome.length() + " letras" + "\n" + " Sua senha começa com caracter = " + novaSenha.charAt(0));
+        if (novoNome.equals("") || novaSenha.equals("")) {
+            ModelAndView mj = new ModelAndView("formulario.html");
+            mj.addObject("erro1","Os dois campos são obrigatorios");
+            return mj;
+        }
+        return mv;
+
+    }
+
+
+}
